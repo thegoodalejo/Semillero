72c07131614ad017c6522359d5ee0d7dc3792a6f
jonathanf03 <jonathanferney103@gmail.com>
2021-06-21 12:26:06 -0500

    v1

---
+++ PageObjectModel/src/test/java/runner/GuruLogin.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+		glue= "src/test/java/com/sophossolutions/stepdefinitions",
+		features= "src/test/resources/com/sophossolutions/features/guru_login.feature",
+		snippets= SnippetType.CAMELCASE)
+public class GuruLogin {
+}
