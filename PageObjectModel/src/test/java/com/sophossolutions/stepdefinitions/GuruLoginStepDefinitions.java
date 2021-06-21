32c0c2ba0c629747a921a84628e5cd30cd309761
Valentina Correa Romero <valentina.correa@sophossolutions.com>
2021-06-21 15:10:20 -0500

    camel

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -1,6 +1,24 @@
 package com.sophossolutions.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class GuruLoginStepDefinitions {
 
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseoIrALaPaginaDe(String string) {
+		System.out.println("@Given");
+	}
+
+	@When("Ingreso nombre de usuario {string} y password {string}")
+	public void ingresoNombreDeUsuarioYPassword(String string, String string2) {
+		System.out.println("@When");
+	}
+
+	@Then("Valido que el titulo sea {string}")
+	public void validoQueElTituloSea(String string) {
+		System.out.println("@Then");
+	}
 
 }
