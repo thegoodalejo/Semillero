3f0c9289407532e10e37c8ee784e616a69ee2a35
juanBlancoSophos <juan.blanco@sophossolutions.com>
2021-06-21 12:23:19 -0500

    Iniciacion del proyecto.

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -1,5 +1,27 @@
 package com.sophossolutions.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class GuruLoginStepDefinitions {
 
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseoIrALaPaginaDe(String string) {
+		System.out.println("@Given");
+	}
+
+	@When("Ingreso el nombre de usuario {string} y password {string}")
+	public void ingresoElNombreDeUsuarioYPassword(String string, String string2) {
+		System.out.println("@When");
+	}
+
+	@Then("Valido que el titulo sea {string}")
+	public void validoQueElTituloSea(String string) {
+	    System.out.println("@Then");
+	}
+
+	
 }
+
+
---
+++ PageObjectModel/src/test/resources/com/sophossolutions/features/guru_login.feature
@@ -0,0 +1,12 @@
+#Author: juan.blanco@sophossolutions.com
+
+Feature: Title of your feature
+  Yo como automatizador quiero entrar a la pagina de Guru
+ 
+ # UserID = mngr335753
+ # Pass = AgYjYnu
+
+  Scenario: Iniciar sesion en Guru
+    Given Deseo ir a la pagina de "http://demo.guru99.com/V4"
+    When Ingreso el nombre de usuario "mngr335753" y password "AgYjYnu"
+    Then Valido que el titulo sea "Manager Id: mngr335753"
