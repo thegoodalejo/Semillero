b3c86f3aabe5f030aa0a8d91b0c0fe54f72952a0
Santiago Andres Espínal Mendoza <santiago.espinal@sophossolutions.com>
2021-06-21 12:20:03 -0500

    v1

---
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -0,0 +1,28 @@
+package com.sophossolutions.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+
+public class GuruLoginStepDefinitions {
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseo_ir_a_la_pagina_de(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Ingreso nombre de usuario {string} y password {string}")
+	public void ingreso_nombre_de_usuario_y_password(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Valido que el tiitulo sea {string}")
+	public void valido_que_el_tiitulo_sea(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+}
