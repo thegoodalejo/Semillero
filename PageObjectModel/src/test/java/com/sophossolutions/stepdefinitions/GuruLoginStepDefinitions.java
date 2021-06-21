d9d9999f6bec37991590603c82cd3ed077f8d9d3
Guacha <guachamalinga@gmail.com>
2021-06-21 11:33:34 -0500

    step definitions agregado

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -1,5 +1,25 @@
 package com.sophossolutions.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class GuruLoginStepDefinitions {
-	
+	@Given("I want to login into {string}")
+	public void iWantToLoginInto(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I input username {string} and password {string}")
+	public void iInputUsernameAndPassword(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I validate the title equals {string}")
+	public void iValidateTheTitleEquals(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
