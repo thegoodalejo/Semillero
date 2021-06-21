8d40e2ccb31012d9cca2a0c3c60919a288e0cfe5
Santiago Andres Espínal Mendoza <santiago.espinal@sophossolutions.com>
2021-06-21 16:04:46 -0500

    v2

--- Semillero/PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ Semillero/PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -4,22 +4,20 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-
-
 public class GuruLoginStepDefinitions {
+	
 	@Given("Deseo ir a la pagina de {string}")
-	public void deseo_ir_a_la_pagina_de(String string) {
-
-		System.out.println("@Given");
+	public void deseoIrALaPaginaDe(String string) {
+	    System.out.println("@Given");
 	}
 
 	@When("Ingreso nombre de usuario {string} y password {string}")
-	public void ingreso_nombre_de_usuario_y_password(String string, String string2) {
+	public void ingresoNombreDeUsuarioYPassword(String string, String string2) {
 		System.out.println("@When");
 	}
 
 	@Then("Valido que el tiitulo sea {string}")
-	public void valido_que_el_tiitulo_sea(String string) {
+	public void validoQueElTiituloSea(String string) {
 		System.out.println("@Then");
 	}
 
