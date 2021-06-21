d9656fb3e148fe8196f934c404ef2db9cb03b0d2
Guacha <guachamalinga@gmail.com>
2021-06-21 12:15:40 -0500

    Removed default exceptions

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -8,18 +8,18 @@ public class GuruLoginStepDefinitions {
 	@Given("I want to login into {string}")
 	public void iWantToLoginInto(String url) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		System.out.println("@Given");
 	}
 
 	@When("I input username {string} and password {string}")
 	public void iInputUsernameAndPassword(String user, String psw) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		System.out.println("@When");
 	}
 
 	@Then("I validate the title equals {string}")
 	public void iValidateTheTitleEquals(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		System.out.println("@Then");
 	}
 }
