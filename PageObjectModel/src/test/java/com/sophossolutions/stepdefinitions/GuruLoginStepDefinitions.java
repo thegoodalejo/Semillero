46f2bdd5ebc0987a81fce159374e39f41063321d
Andres Rodrigo Lopez Realpe <andres.lopez@sophossolutions.com>
2021-06-21 20:02:20 -0500

    Actualizacion 3

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -1,5 +1,26 @@
 package com.sophossolutions.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class GuruLoginStepDefinitions {
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseo_ir_a_la_pagina_de(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Ingreso Nombre de Usuario {string} y password {string}")
+	public void ingreso_Nombre_de_Usuario_y_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I validate the outcomes {string}")
+	public void i_validate_the_outcomes(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
