42bde17adc4eaecfba4effad60b4dd0b06a03330
Santiago Andres Espínal Mendoza <santiago.espinal@sophossolutions.com>
2021-06-22 10:24:39 -0500

    v3

---
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -0,0 +1,29 @@
+package com.sophossolutions.stepdefinitions;
+
+import com.sophossolutions.pages.GuroLogin;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GuruLoginStepDefinitions {
+
+	GuroLogin loginGuru;
+	
+	@Given("Deseo ir a la pagina de {string}")
+	public void deseoIrALaPaginaDe(String url) {
+		System.out.println("@Given");
+		loginGuru.navegateTo(url);
+	}
+
+	@When("Ingreso nombre de usuario {string} y password {string}")
+	public void ingresoNombreDeUsuarioYPassword(String string, String string2) {
+		System.out.println("@When");
+	}
+
+	@Then("Valido que el titulo sea {string}")
+	public void validoQueElTituloSea(String string) {
+		System.out.println("@Then");
+	}
+
+}
