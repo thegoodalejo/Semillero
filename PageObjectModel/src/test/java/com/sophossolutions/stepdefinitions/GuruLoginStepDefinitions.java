9b825a3b61549903786be2f210a1e9a8e444bdb7
Andres Rodrigo Lopez Realpe <andres.lopez@sophossolutions.com>
2021-06-21 20:08:17 -0500

    Actualizacion 4

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -5,22 +5,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GuruLoginStepDefinitions {
+
 	@Given("Deseo ir a la pagina de {string}")
-	public void deseo_ir_a_la_pagina_de(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void deseoIrALaPaginaDe(String string) {
+		System.out.println("@Given");
 	}
 
-	@When("Ingreso Nombre de Usuario {string} y password {string}")
-	public void ingreso_Nombre_de_Usuario_y_password(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@When("Ingreso nombre de usuario {string} y password {string}")
+	public void ingresoNombreDeUsuarioYPassword(String string, String string2) {
+		System.out.println("@When");
 	}
 
-	@Then("I validate the outcomes {string}")
-	public void i_validate_the_outcomes(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@Then("Valido que el titulo sea {string}")
+	public void validoQueElTituloSea(String string) {
+		System.out.println("@Then");
 	}
 
 }
