682dd5301617c75a1ae2d70dece7762a95be30fa
Andres Rodrigo Lopez Realpe <andres.lopez@sophossolutions.com>
2021-06-22 10:18:44 -0500

    Actualizacion 5.3

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/GuruLoginStepDefinitions.java
@@ -3,13 +3,17 @@ package com.sophossolutions.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import com.sophossolutions.pages.GuroLogin;
 
 public class GuruLoginStepDefinitions {
+	GuroLogin loginGuru;
 
 	@Given("Deseo ir a la pagina de {string}")
-	public void deseoIrALaPaginaDe(String string) {
+	public void deseoIrALaPaginaDe(String url) {
 		System.out.println("@Given");
-	}
+		loginGuru.navegateTo(url);
+		
+	}	
 
 	@When("Ingreso nombre de usuario {string} y password {string}")
 	public void ingresoNombreDeUsuarioYPassword(String string, String string2) {
