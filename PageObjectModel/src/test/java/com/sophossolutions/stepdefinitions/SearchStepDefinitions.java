cfe37deb1b7819d5b313c1e42ec6952231493468
Guacha <guachamalinga@gmail.com>
2021-06-23 09:31:34 -0500

    Project Refactoring

--- PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/SearchStepDefinitions.java
+++ PageObjectModel/src/test/java/com/sophossolutions/stepdefinitions/SearchStepDefinitions.java
@@ -13,14 +13,14 @@ public class SearchStepDefinitions {
 	AmazonSearchPage results;
 	
 	@Given("Quiero buscar {string} en https:\\/\\/www.amazon.com")
-	public void quieroBuscarEnHttpsWwwAmazonCom(String string) {
+	public void quieroBuscarEnHttpsWwwAmazonCom(String strSearch) {
 	    main.navigateTo("https://www.amazon.com");
 	}
 
 
 	@When("Ingreso {string} en la barra de búsqueda y procedo a buscar")
-	public void ingresoEnLaBarraDeBúsquedaYProcedoABuscar(String search) {
-	    main.search(search);
+	public void ingresoEnLaBarraDeBúsquedaYProcedoABuscar(String strSearch) {
+	    main.search(strSearch);
 	}
 
 	@Then("Debería ver al menos {int} productos y sus precios que se relacionen con la palabra {string}")
